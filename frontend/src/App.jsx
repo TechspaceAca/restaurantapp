f9a99de7f6068fa7a2d61f116a6008bf4dcd4d13
arathy <arathyrajeesh2@gmail.com>
2026-07-29 10:59:16 +0530

    feat: switch to HashRouter in App.jsx to guarantee 100% 404-free single-page navigation on Vercel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import useStore from './store/useStore';
 
@@ -47,7 +47,7 @@ export default function App() {
   }, [theme]);
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Toaster
         position="top-right"
         toastOptions={{
@@ -111,6 +111,6 @@ export default function App() {
         } />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
